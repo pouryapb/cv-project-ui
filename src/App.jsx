8126aaf3e04caf3298f27dae41e5acd1c9f55d45
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-06-10 02:30:59 +0430

    file select handler (not finished)

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import {
   Paper,
   Select,
   ThemeProvider,
+  Typography,
 } from "@material-ui/core";
 import { CloudUpload } from "@material-ui/icons";
 
@@ -27,6 +28,9 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     height: "100%",
+  },
+  filesContainer: {
+    padding: theme.spacing(2),
   },
 }));
 
@@ -45,10 +49,17 @@ const formats = [
 
 function App() {
   const classes = useStyles();
-  const [format, setFormat] = useState("jpeg");
+  const [format, setFormat] = useState("");
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
   const handleSelect = (event) => {
     setFormat(event.target.value);
+  };
+
+  const handleFileSelected = (event) => {
+    const files = Array.from(event.target.files);
+    console.log("files:", files);
+    setSelectedFiles(files);
   };
 
   return (
@@ -66,7 +77,13 @@ function App() {
                 component="label"
               >
                 Load Image
-                <input accept="image/*" type="file" hidden multiple />
+                <input
+                  accept="image/*"
+                  type="file"
+                  hidden
+                  multiple
+                  onChange={handleFileSelected}
+                />
               </Button>
             </Grid>
             <Grid item>
@@ -77,17 +94,25 @@ function App() {
                   id="format-select-label"
                   value={format}
                   onChange={handleSelect}
-                  label="Format"
                 >
-                  {formats.map((format, index) => (
-                    <MenuItem key={index} value={format}>
-                      {format}
-                    </MenuItem>
-                  ))}
+                  <div className={classes.filesContainer}>
+                    {formats.map((format, index) => (
+                      <MenuItem key={index} value={format}>
+                        {format}
+                      </MenuItem>
+                    ))}
+                  </div>
                 </Select>
               </FormControl>
             </Grid>
           </Grid>
+          {selectedFiles.map((file, index) => {
+            return (
+              <Typography key={index}>
+                {index + 1 + ". " + file.name}
+              </Typography>
+            );
+          })}
         </Paper>
       </Container>
     </ThemeProvider>
